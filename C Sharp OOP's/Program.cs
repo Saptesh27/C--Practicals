05bfb44deabc3fb08bb80a2820df21a9154cdda3
Saptesh <68159984+Saptesh27@users.noreply.github.com>
2023-01-31 09:44:24 +0530

    Method Overloading

---
+++ C Sharp OOP's/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    class demo
+    {
+        public void sum(int a, int b)
+        { 
+            Console.WriteLine("Addition is "+(a+b));        
+        }
+        public void sum(int a, int b, int c)
+        { 
+         Console.WriteLine("Addition is "+(a+b+c));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo d1 = new demo();
+            d1.sum(10, 10);
+            d1.sum(10, 10, 10);
+            Console.ReadLine();
+        }
+    }
+}
