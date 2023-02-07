1b6f3e69c952670045107633fcb1ae799cdc069d
Saptesh <68159984+Saptesh27@users.noreply.github.com>
2023-02-07 08:55:59 +0530

    Method Overriding using VIRTUAL and BASE keyword

---
+++ C Sharp OOP's/MethodOverriding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Method_Overriding
+{
+    class demo
+
+    {
+        public virtual void show() //virtual keyword in parent class
+
+        {
+            Console.WriteLine("Base Class...");        
+        }
+    }
+
+    class demo1 : demo
+    {
+        public override void show() //base keyword in child class
+        {
+            Console.WriteLine("Derived  class...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo1 d1 = new demo1();
+            d1.show();
+            Console.ReadLine();
+        }
+    }
+}
