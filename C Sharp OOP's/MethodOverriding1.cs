7360e16df1402bf66ccffe7dcfc85c68b8e0391a
Saptesh <68159984+Saptesh27@users.noreply.github.com>
2023-02-07 09:00:45 +0530

    Method Overriding using BASE keyword.

---
+++ C Sharp OOP's/MethodOverriding1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Method_Overriding
+{
+    class demo
+
+    {
+        public void show() 
+
+        {
+            Console.WriteLine("Base Class...");        
+        }
+    }
+
+    class demo1 : demo
+    {
+
+        
+        public void show() 
+        {
+            base.show();
+            Console.WriteLine("Derived  class...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo1 d1 = new demo1();
+            d1.show();
+            Console.ReadLine();
+        }
+    }
+}
