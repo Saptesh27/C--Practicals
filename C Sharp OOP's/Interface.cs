3651d72128bfc071c9c0ceeb7000e95e937f9d61
Saptesh <68159984+Saptesh27@users.noreply.github.com>
2023-02-07 09:31:40 +0530

    Interface Program

---
+++ C Sharp OOP's/Interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface_Program
+{
+    interface demo //Declaration of interface
+    {
+        void show(); //abstract Method Declaration
+    }
+    class demo1 : demo //Inheriting interface or implementing it
+    {
+        public void show() //Defination of abstract method
+        {
+            Console.WriteLine("Demostration Of Interface");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo1 d1 = new demo1();
+            d1.show();
+            Console.ReadLine();
+        }
+    }
+}
