1311a74f29ed24e31c6b2b450560eb4c42bacf3d
Saptesh <68159984+Saptesh27@users.noreply.github.com>
2023-03-14 10:23:03 +0530

    timer

---
+++ timer1.cs/timer1.cs/form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace timer1.cs
+{
+    public partial class form2 : Form
+    {
+        public form2()
+        {
+            InitializeComponent();
+        }
+
+        private void form2_Load(object sender, EventArgs e)
+        {
+           
+
+            
+        }
+    }
+}
